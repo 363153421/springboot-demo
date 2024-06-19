7ca753c36c9fa1eecf0733dc3df6975101f96341
wukefan <363153421@qq.com>
2024-06-19 13:46:12 +0800

    minio

---
+++ minio/src/main/java/com/wkf/minio/MinioApplication.java
@@ -0,0 +1,17 @@
+package com.wkf.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuKeFan
+ * @date 2024-06-13 09:51:13
+ */
+@SpringBootApplication
+public class MinioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioApplication.class, args);
+    }
+
+}
